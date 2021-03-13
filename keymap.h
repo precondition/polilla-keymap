49a8e894d62beba22bd0dc74408ca5528d66ce93
precondition <57645186+precondition@users.noreply.github.com>
2021-03-13 09:44:10 +0100

    Ported Dactyl Manuform keymap to Polilla

---
+++ keymap.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+    _COLEMAK_DH,
+    _SYM,
+    _NAV,
+    _GNAV,
+    _MOUSE,
+};
+
+// Layer keys
+#define NAV_TAB LT(_NAV, KC_TAB)
+#define GNAV MO(_GNAV)
+#define SYM MO(_SYM)
+#define MOUSE MO(_MOUSE)
+#define MS_CAPS LT(_MOUSE, KC_CAPS)
+#define SYM_ENT LT(_SYM, KC_ENT)
+#define NAV_UND LT(_NAV, KC_F24)
+
+// Miscellaneous keyboard shortcuts in direct access
+#define UNDO LCTL(KC_Z)
+#define REDO LCTL(KC_Y)
+#define COMPOSE KC_APP
+#define TG_MIC KC_F20 // Default binding for XF86AudioMicMute
+
+// Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_R LALT_T(KC_R)
+#define HOME_S LSFT_T(KC_S)
+#define HOME_T LCTL_T(KC_T)
+
+// Right-hand home row mods
+#define HOME_O RGUI_T(KC_O)
+#define HOME_I LALT_T(KC_I)
+#define HOME_E RSFT_T(KC_E)
+#define HOME_N RCTL_T(KC_N)
+
+// One Shot Shifts
+#define OS_LSFT OSM(MOD_LSFT)
+#define OS_RSFT OSM(MOD_RSFT)
+
+// French accents
+// The other common grave accented letters are custom keycodes
+// And the rest of other accented letters are accessed using dead keys
+#define E_ACUTE ALGR(KC_E)
+#define C_CDILA ALGR(KC_COMMA)
+// Useful dead keys for French
+#define DED_CIR ALGR(KC_6)
+#define DED_UML S(ALGR(KC_QUOT))
+
+enum custom_keycodes {
+    ARROW_R = SAFE_RANGE,
+    // g navigation to move through screen lines in Vim
+    G_DOWN,
+    G_UP,
+    G_HOME,
+    G_END,
+    // « French quotation marks »
+    GUILL_R,
+    GUILL_L,
+    // Macro to refer to the parent directory in Linux ../
+    UPDIR,
+    // French grave accented letters
+    E_GRAVE,
+    A_GRAVE,
+    // "Yank Inner CODE"
+    // Macro for
+    // $yi{
+    YICODE,
+    CAPS_WORD,
+};
+
+#ifdef TAP_DANCE_ENABLE
+enum {
+    DOT_TD = 0,
+    CA_CC_CV,
+};
+
+#define TD_DOT TD(DOT_TD)
+#define CACCCV TD(CA_CC_CV)
+#else
+#define TD_DOT KC_DOT
+#endif
---
+++ rules.mk
@@ -0,0 +1,19 @@
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+UNICODEMAP_ENABLE = no
+KEY_LOCK_ENABLE = no
+MOUSEKEY_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+NKRO_ENABLE = no
+LTO_ENABLE = no
+BOOTMAGIC_ENABLE = no
+
+# Load the necessary external C files if and only if
+# the associated config option has been enabled
+ifeq ($(strip $(COMBO_ENABLE)), yes)
+	SRC += combos.c
+endif
+ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
+	SRC += tapdance.c
+endif
