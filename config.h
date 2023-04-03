311c5c9dfa5066cbe0282cd5e4428c6eb60a3488
precondition <57645186+precondition@users.noreply.github.com>
2023-04-03 12:24:32 +0200

    Remove obsolete IGNORE_MOD_TAP_INTERRUPT

--- config.h
+++ config.h
@@ -6,9 +6,6 @@
 #define TAPPING_TERM 170
 #define TAPPING_TERM_PER_KEY // See bottom of keymap.c
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
-
 // This prevents accidental repeats of the tap-hold keys when typing quickly.
 #define TAPPING_FORCE_HOLD_PER_KEY
 #define QUICK_TAP_TERM_PER_KEY
