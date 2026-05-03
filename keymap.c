a48d4f7f3d22e3093c4a42f2d8b2c15223e33b5a
precondition <57645186+precondition@users.noreply.github.com>
2026-05-03 22:39:33 +0200

    Do not escape dead keys with a space if altgr is active
    This mimics the behavior of US intl. (with AltGr dead keys), found on
    Linux.

--- keymap.c
+++ keymap.c
@@ -654,7 +654,7 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_TILDE:
         case KC_GRAVE:
         case KC_CIRCUMFLEX:
-            if (base_dead_keys && record->event.pressed) {
+            if (base_dead_keys && record->event.pressed && !(get_mods() & MOD_RALT)) {
                 tap_code(KC_SPACE);
             }
             break;
