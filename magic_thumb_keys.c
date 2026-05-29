2ca6f735ee87dca814d7bb41b31e5df649f151a9
precondition <57645186+precondition@users.noreply.github.com>
2026-05-29 08:17:02 +0200

    Big magic_thumb_keys.c update

--- magic_thumb_keys.c
+++ magic_thumb_keys.c
@@ -34,15 +34,21 @@ void process_magic_key_left(uint16_t prev_keycode, uint16_t prev_prev_keycode) {
             break;
 
         case KC_C:
-            // rationale: avoid SFS.
-            // ngram: "oca" (0.01074%)
-            // examples: "local", "vocal", "vocab", "allocates"
-            /*
-             * "oca" typically occurs in the middle of a word so it is unlikely
-             * to cause a left thumb space SFB → good fit for MAGIC_L.
-             */
-            tap_code(KC_A);
-            last_summoned_keycode = KC_A;
+            // rationale: avoid SFB.
+            // ngram: "cs" (0.03134%)
+            // examples: "trucs", "docs", "csv", "css", "ergonomics"
+            tap_code(KC_S);
+            last_summoned_keycode = KC_S;
+            break;
+
+        case KC_E:
+            // rationale: avoid SFB.
+            // ngram: "e<sym>" (0.10403%)
+            // examples: TODO
+            tap_code(KC_I);
+            tap_code(KC_T);
+            tap_code(KC_SPACE);
+            last_summoned_keycode = KC_SPACE;
             break;
 
         case KC_G:
@@ -53,6 +59,16 @@ void process_magic_key_left(uint16_t prev_keycode, uint16_t prev_prev_keycode) {
             tap_code(KC_T);
             tap_code(KC_SPACE);
             last_summoned_keycode = KC_SPACE;
+            break;
+
+        case KC_I:
+        case HOME2_I:
+            // rationale: avoid SFB.
+            // ngram: "ize" (0.03639%)
+            // examples: "size",  "realize",  "capitalize",  "optimized"
+            tap_code(KC_Z);
+            tap_code(KC_E);
+            last_summoned_keycode = KC_E;
             break;
 
         case KC_N:
@@ -83,6 +99,14 @@ void process_magic_key_left(uint16_t prev_keycode, uint16_t prev_prev_keycode) {
             // examples: "compte",  "option",  "script",  "ptet",  "except"
             tap_code(KC_T);
             last_summoned_keycode = KC_T;
+            break;
+
+        case KC_Q:
+            // rationale: TODO
+            // ngram: "q!" (TODO%)
+            // examples: ":q!⏎"
+            tap_code16(KC_EXLM);
+            last_summoned_keycode = KC_EXLM;
             break;
 
         case KC_R:
@@ -124,7 +148,7 @@ void process_magic_key_left(uint16_t prev_keycode, uint16_t prev_prev_keycode) {
 
         case KC_Y:
             // rationale: avoid SFB.
-            // ngram: "ying␣" (TODO%)
+            // ngram: "yi" (TODO%)
             // examples: "annoying␣",  "saying␣",  "buying␣"
             /*
              * "yi" is almost always followed by "ng" ("ying" makes up 0.68375%
@@ -133,20 +157,25 @@ void process_magic_key_left(uint16_t prev_keycode, uint16_t prev_prev_keycode) {
              * commands (0.24863%) like "yiw".
              */
             tap_code(KC_I);
-            tap_code(KC_N);
-            tap_code(KC_G);
-            tap_code(KC_SPACE);
-            last_summoned_keycode = KC_SPACE;
+            last_summoned_keycode = KC_I;
+            break;
+
+        case KC_Z:
+            // rationale: avoid SFB.
+            // ngram: "zy" (0.00167%)
+            // examples: "crazy",  "lazy",  "fuzzy",  "cozy"
+            tap_code(KC_Y);
+            last_summoned_keycode = KC_Y;
             break;
 
         case KC_QUOTE:
             // rationale: avoid SFB.
-            // ngram: "'m␣" (0.01094%)
-            // examples: "I'm not",  "I'm still",  "I'm trying"
-            tap_code(KC_M);
-            tap_code(KC_SPACE);
-            last_summoned_keycode = KC_SPACE;
+            // ngram: "'⏎" and ""⏎" (0.02988%)
+            // examples: "git commit -m "msg"⏎", "vim 'copy\pasted\windows\path'⏎"
+            tap_code16(KC_ENTER);
+            last_summoned_keycode = KC_ENTER;
             break;
+
 
         case KC_MINUS:
             // rationale: avoid SFS.
@@ -162,18 +191,46 @@ void process_magic_key_left(uint16_t prev_keycode, uint16_t prev_prev_keycode) {
             last_summoned_keycode = KC_MINUS;
             break;
 
-        default:
-            // Report the unsupported leading key by typing out its key code in
-            // decimal format.
-            const char *prev_keycode_str = get_u16_str(prev_keycode, ' ');
-            // Skip padding spaces
-            while (*prev_keycode_str == ' ') {
-                prev_keycode_str++;
-            }
-            send_string(prev_keycode_str);
-            tap_code16(KC_QUESTION);
-            last_summoned_keycode = KC_QUESTION;
+        case KC_RPRN:
+            // rationale: reduce typing.
+            // ngram: ");" (TODO%)
+            // examples: TODO
+            tap_code(KC_SEMICOLON);
+            last_summoned_keycode = KC_SEMICOLON;
             break;
+
+        case QK_REP:
+            // rationale: avoid SFB.
+            // ngram: "↻r" (TODO%)
+            // examples: TODO
+            tap_code(KC_R);
+            last_summoned_keycode = KC_R;
+            break;
+
+        case MAGIC_R:
+        case LT(_ALTTABCP, MAGIC_R):
+            // rationale: avoid SFB.
+            // ngram: "<MAGIC_R>e"
+            switch (last_summoned_keycode) {
+                default:
+                    tap_code(KC_E);
+                    last_summoned_keycode = KC_E;
+                    break;
+            }
+            break;
+
+        //default:
+        //    // Report the unsupported leading key by typing out its key code in
+        //    // decimal format.
+        //    const char *prev_keycode_str = get_u16_str(prev_keycode, ' ');
+        //    // Skip padding spaces
+        //    while (*prev_keycode_str == ' ') {
+        //        prev_keycode_str++;
+        //    }
+        //    send_string(prev_keycode_str);
+        //    tap_code16(KC_QUESTION);
+        //    last_summoned_keycode = KC_QUESTION;
+        //    break;
     }
 }
 
@@ -181,10 +238,20 @@ void process_magic_key_left(uint16_t prev_keycode, uint16_t prev_prev_keycode) {
 void process_magic_key_right(uint16_t prev_keycode, uint16_t prev_prev_keycode) {
     switch (prev_keycode) {
 
+        case KC_A:
+        case HOME2_A:
+            // rationale: avoid SFS.
+            // ngram: "dat" (0.07028%), "tad" (0.00412%)
+            // examples: "data", "date", "update", "stade"
+            tap_code(KC_T);
+            last_summoned_keycode = KC_T;
+            break;
+
         case KC_C:
-            // rationale: avoid SFB.
-            // ngram: "cs" (0.03134%)
-            // examples: "trucs", "docs", "csv", "css", "ergonomics"
+            // rationale: avoid SFS.
+            // ngram: "ces" (0.03377%)
+            // examples: "␣ces␣",  "access",  "exercices",  "process"
+            tap_code(KC_E);
             tap_code(KC_S);
             last_summoned_keycode = KC_S;
             break;
@@ -193,8 +260,23 @@ void process_magic_key_right(uint16_t prev_keycode, uint16_t prev_prev_keycode) 
             // rationale: avoid SFB.
             // ngram: "gs" (0.01907%)
             // examples: "settings", "things", "strings", "mappings"
-            tap_code(KC_S);
-            last_summoned_keycode = KC_S;
+            if (prev_prev_keycode == KC_SLASH) {
+                tap_code(KC_C);
+                last_summoned_keycode = KC_C;
+            } else {
+                tap_code(KC_S);
+                last_summoned_keycode = KC_S;
+            }
+            break;
+
+        case KC_I:
+        case HOME2_I:
+            // rationale: avoid SFS.
+            // ngram: "ity" (0.05076%)
+            // examples: "functionality",  "ability",  "community"
+            tap_code(KC_T);
+            tap_code(KC_Y);
+            last_summoned_keycode = KC_Y;
             break;
 
         case KC_O:
@@ -203,6 +285,14 @@ void process_magic_key_right(uint16_t prev_keycode, uint16_t prev_prev_keycode) 
             // examples: "how",  "know",  "down",  "now",  "Windows"
             tap_code(KC_W);
             last_summoned_keycode = KC_W;
+            break;
+
+        case KC_P:
+            // rationale: avoid SFB.
+            // ngram: "pd" (0.01643%)
+            // examples: "update",  "pdf",  "pdt",  "dropdown",  "pd.DataFrame"
+            tap_code(KC_D);
+            last_summoned_keycode = KC_D;
             break;
 
         case KC_R:
@@ -229,6 +319,15 @@ void process_magic_key_right(uint16_t prev_keycode, uint16_t prev_prev_keycode) 
             last_summoned_keycode = KC_G;
             break;
 
+        case KC_T:
+        case HOME2_T:
+            // rationale: avoid SFB.
+            // ngram: "tp" (0.01311%)
+            // examples: "output",  "smartphone",  "stp" (s'il te plaît)
+            tap_code(KC_P);
+            last_summoned_keycode = KC_P;
+            break;
+
         case KC_W:
             // rationale: avoid ring-pinky outer row skip.
             // ngram: "wo" (0.07580%)
@@ -237,6 +336,13 @@ void process_magic_key_right(uint16_t prev_keycode, uint16_t prev_prev_keycode) 
             last_summoned_keycode = KC_O;
             break;
 
+        case KC_U:
+            // rationale: avoid SFS.
+            // ngram: "put" (0.10564%), "but", (0.03200%)"dut" (0.00250%)
+            // examples: "but",  "put",  "input",  "début",  "computer", "dutch"
+            tap_code(KC_T);
+            last_summoned_keycode = KC_T;
+            break;
 
         case KC_Y:
             // rationale: avoid SFB.
@@ -285,8 +391,17 @@ void process_magic_key_right(uint16_t prev_keycode, uint16_t prev_prev_keycode) 
             last_summoned_keycode = KC_GT;
             break;
 
+        case KC_QUOTE:
+            // rationale: avoid SFB.
+            // ngram: "'m␣" (0.01094%)
+            // examples: "I'm not",  "I'm still",  "I'm trying"
+            tap_code(KC_M);
+            tap_code(KC_SPACE);
+            last_summoned_keycode = KC_SPACE;
+            break;
 
         case MAGIC_L:
+        case LT(_ALTTABCP, MAGIC_L):
             // rationale: avoid SFB.
             // ngram: "_<MAGIC_L>␣", "__<MAGIC_L>␣"
             /*
@@ -298,13 +413,16 @@ void process_magic_key_right(uint16_t prev_keycode, uint16_t prev_prev_keycode) 
              * alleviate the "<MAGIC_L>␣" SFB. The "<MAGIC_L>_␣" SFS cannot be
              * bypassed though.
              */
-            if (last_summoned_keycode == KC_SPACE) {
-                // Avoid the KC_G MAGIC_L KC_C SFS.
-                SEND_STRING("commit ");
-                last_summoned_keycode = KC_SPACE;
-            } else {
-                tap_code(KC_SPACE);
-                last_summoned_keycode = KC_SPACE;
+            switch (last_summoned_keycode) {
+                case KC_SPACE:
+                    // Avoid the KC_G MAGIC_L KC_C SFS.
+                    SEND_STRING("commit ");
+                    last_summoned_keycode = KC_SPACE;
+                    break;
+                default:
+                    tap_code(KC_SPACE);
+                    last_summoned_keycode = KC_SPACE;
+                    break;
             }
             break;
 
